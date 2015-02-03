85d9b201d83897572b64b9e07e6cd49b16e2aa49
Coleman Stavish <ccs@colemanstavish.com>
2015-02-03 11:18:18 -0500

    recursively print trees

--- id3.py
+++ id3.py
@@ -3,35 +3,53 @@
 # Coleman Stavish
 
 import sys
+import math
 from collections import Counter
 
 # define gain functions
 
-def entropy():
+def entropy(S):
+	entropy = 0
+	labels = [e['label'] for e in S]
+	cnt = Counter(labels)
+	for label in cnt.keys():
+		p_label = float(cnt[label]) / len(S)
+		entropy += (-p_label * math.log(p_label, 2))
+	return entropy
+
+def misclassification(S):
 	return
 
-def misclassification():
-	return
-
-def gini():
+def gini(S):
 	return
 
 class ID3_Tree:
-	def __init__(self, labels, features, examples, gain_func):
+	def __init__(self, labels, features, examples, diversity_func):
 		self.labels = labels
 		self.features = features
 		self.examples = examples
-		self.gain_func = gain_func
+		self.diversity_func = diversity_func
+
+	def _gain(self, feature, examples):
+		entropy = self.diversity_func(examples)
+		diversity_sum = 0
+		for v in feature['values']:
+			Sv = []
+			for e in examples:
+				if e['values'][feature['name']] == v:
+					Sv.append(e)
+			diversity_sum += self.diversity_func(Sv) * (float(len(Sv))/len(examples))
+		return entropy - diversity_sum
 
 	def _train(self, examples, features):
-		root = {'children:' {}}
+		root = {}
 
 		# if all examples have the same label
 			# return root as leaf node with that label
-		all_identical = true
+		all_identical = True
 		for i in xrange(1, len(examples)):
 			if examples[0]['label'] != examples[i]['label']:
-				all_identical = false
+				all_identical = False
 				break
 
 		if all_identical:
@@ -42,13 +60,16 @@ class ID3_Tree:
 
 		if len(features) == 0:
 			labels = [e['label'] for e in examples]
-			return Counter(labels).most_common(1)
+			return Counter(labels).most_common(1)[0][0]
 
 		# compute gain() for all features
 		# associate root with xi* (highest gain)
 
-		gain_values = [(f, self.gain_func(f, examples)) for f in features]
+		gain_values = [(f, self._gain(f, examples)) for f in features]
 		xi_star = max(gain_values, key=lambda x: x[1])[0]
+
+		root['feature_name'] = xi_star['name']
+		root['children'] = {}
 
 		# for each possible value of xi*:
 			# find Sv (subset of examples where xi*=v)
@@ -69,12 +90,26 @@ class ID3_Tree:
 				root['children'][v] = subtree
 			else:
 				labels = [e['label'] for e in examples]
-				root['children'][v] = Counter(labels).most_common(1)
+				root['children'][v] = Counter(labels).most_common(1)[0][0]
 
+		#print 'returning root ', root
 		return root
 
 	def train(self):
-		return self._train(self.examples, self.features)
+		self.root = self._train(self.examples, self.features)
+
+	def print_tree(self, root, level):
+		if root == None:
+			root = self.root
+		if type(root) is str:
+			print 'root = ', root
+			return
+		for child in root['children'].keys():
+			if type(root['children'][child]) is str:
+				print ('\t' * level) + root['feature_name'] + '=' + child + ' ' + str(root['children'][child])
+			else:
+				print ('\t' * level) + root['feature_name'] + '=' + child
+				self.print_tree(root['children'][child], level + 1)
 
 	def classify(self, object):
 		return
@@ -83,18 +118,18 @@ def main():
 	features = []
 	examples = []
 	labels = []
-	gain_func = None
+	diversity_func = None
 
 	if len(sys.argv) < 5:
 		print '{0}: arguments required.'.format(sys.argv[0])
 		sys.exit(1)
 
 	if sys.argv[1] == 'entropy':
-		gain_func = entropy
+		diversity_func = entropy
 	elif sys.argv[1] == 'misclassification':
-		gain_func = misclassification
+		diversity_func = misclassification
 	elif sys.argv[1] == 'gini':
-		gain_func = gini
+		diversity_func = gini
 	else:
 		print '{0}: invalid gain function (must choose `entropy`, `misclassification`, or `gini`.'.format(sys.argv[0])
 		sys.exit(1)
@@ -122,8 +157,10 @@ def main():
 			examples.append(example)
 		train_f.close()
 
-	tree = ID3_Tree(labels, features, examples, gain_func);
+	tree = ID3_Tree(labels, features, examples, diversity_func);
 	tree.train()
+	tree.print_tree(tree.root, 0)
+
 
 if __name__ == '__main__':
 	main()
